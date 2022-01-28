564ca6679146474c44ff8e43adca4a99ae6e8e72
Swapnil Sarwe <swapnilsarwe@gmail.com>
2022-01-29 00:31:08 +0530

    added mix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <title>Pushpa "Flower Samjhe Kya" Memes</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 
 <body class="antialiased">
