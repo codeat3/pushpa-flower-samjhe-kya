ab23731223e01579dabdcf71282819c851266241
Swapnil Sarwe <swapnilsarwe@gmail.com>
2022-01-29 18:31:23 +0530

    save igst

--- app/Http/Controllers/MemeController.php
+++ app/Http/Controllers/MemeController.php
@@ -10,35 +10,47 @@ use Illuminate\Support\Facades\Response;
 
 class MemeController extends Controller
 {
-    private function getDialogue($slug) {
+    private function getDialogue($slug)
+    {
         $randomDialogue = Dialogue::whereSlug($slug)->first();
-        if(empty($randomDialogue)) {
+        if (empty($randomDialogue)) {
             $randomDialogue = Dialogue::inRandomOrder()->first();
         }
+        return $randomDialogue;
         return [
-            Str::before($randomDialogue->dialogue, '? '). '?',
+            Str::before($randomDialogue->dialogue, '? ') . '?',
             Str::after($randomDialogue->dialogue, '? '),
         ];
     }
 
+    private function getFileName($slug)
+    {
+    }
+
     public function __invoke(Request $request, $slug)
     {
-        list($intro, $end) = $this->getDialogue($slug);
-        $img = Image::make(storage_path('images/pushpa.jpg'));
-        $img->text($intro, 500, 400, function ($font) {
-            $font->file(storage_path('fonts/font.ttf'));
-            $font->size(32);
-            $font->align('center');
-            $font->color([255, 255, 255, 1]);
-        });
-        $img->text($end, 500, 830, function ($font) {
-            $font->file(storage_path('fonts/font.ttf'));
-            $font->size(32);
-            $font->align('center');
-            $font->color([255, 255, 255, 1]);
-        });
-      return response()->stream(function() use($img) {
+        $randomDialogue = $this->getDialogue($slug);
+        $intro = Str::before($randomDialogue->dialogue, '? ') . '?';
+        $end = Str::after($randomDialogue->dialogue, '? ');
+        if (!file_exists(storage_path('images/' . $randomDialogue->slug . '.jpg'))) {
+            $img = Image::make(storage_path('images/pushpa.jpg'));
+            $img->text($intro, 500, 400, function ($font) {
+                $font->file(storage_path('fonts/font.ttf'));
+                $font->size(32);
+                $font->align('center');
+                $font->color([255, 255, 255, 1]);
+            });
+            $img->text($end, 500, 830, function ($font) {
+                $font->file(storage_path('fonts/font.ttf'));
+                $font->size(32);
+                $font->align('center');
+                $font->color([255, 255, 255, 1]);
+            });
+            $img->save(storage_path('images/' . $randomDialogue->slug . '.jpg'), 100);
+        }
+        $img = Image::make(storage_path('images/' . $randomDialogue->slug . '.jpg'));
+        return response()->stream(function () use ($img) {
             echo $img->stream('jpg', 100);
-        },200,['Content-Type' => 'image/jpg']);
+        }, 200, ['Content-Type' => 'image/jpg']);
     }
 }
